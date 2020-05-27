92aa0dbd0e2d55c8f3a44a81b833fa5091fe32b0
Chris Walter <walt50@gmail.com>
2020-05-26 21:53:40 -0400

    Initial Commit

---
+++ NeverForgetNames/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeverForgetNames
+//
+//  Created by Christopher Walter on 5/26/20.
+//  Copyright © 2020 Christopher Walter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
